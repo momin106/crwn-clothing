accdb8fd14356d5bfe2378b880fe52b9fa5c9f98
Momin <mominshahzad106@gmail.com>
2021-09-26 17:24:19 +0500

    - Add react hooks in components - Project restructure - Add node js server

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+import './stripe-button.styles.scss';
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey = 'pk_test_51Jdu15DMIIHPsW8RZHQhjbmBDFKS5mtiYVvQxbhqGJFzWGDchoteFcBsA0SoFXCzH3rUYwHDnND8H8ecdQOmBEg300gdQHDStC';
+
+	const onToken = (token) => {
+		axios({
+			url: 'payment',
+			method: 'post',
+			data: {
+				amount: priceForStripe,
+				token,
+			},
+		})
+			.then((resonse) => {
+				alert('Payment successful');
+			})
+			.catch((error) => {
+				console.log('Payment error:', JSON.parse(error));
+				alert('There was an issue with your payment. Please sure you use the provided credit card.');
+			});
+	};
+
+	return (
+		<StripeCheckout
+			label='Pay Now'
+			name='CRWN Clothing Ltd.'
+			billingAddress
+			shippingAddress
+			image='https://svgshare.com/i/CUz.svg'
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel='Pay Now'
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+export default StripeCheckoutButton;
